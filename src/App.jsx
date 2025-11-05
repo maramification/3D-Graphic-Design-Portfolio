b0cf714e6bfa0ae1ec20d6e0d71f9be271d33d14
Maram <maram.hossam.a@gmail.com>
2025-11-05 20:27:08 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+  //App component is an arrow function
+  <div className="container mx-auto max-w-7xl">
+  {/* Navbar */}
+  </div>;
+};
+
+export default App; // makes this exportable/resusable in other files
