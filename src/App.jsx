d7a7d62d981219847cbd644e6cdd37cbfa8b72a7
Maram <maram.hossam.a@gmail.com>
2025-11-10 22:19:40 +0200

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Navbar from "./sections/Navbar";
 
 const App = () => {
   //App component is an arrow function
-  <div className="container mx-auto max-w-7xl">
-  {/* Navbar */}
-  </div>;
+  return (  <div className="container mx-auto max-w-7xl">
+  <Navbar />
+  </div>
+  );
+
 };
 
 export default App; // makes this exportable/resusable in other files
