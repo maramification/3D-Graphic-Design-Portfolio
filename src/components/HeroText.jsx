6fecbe3069bd5cb9fd86537b3751884a436be56b
Maram <maram.hossam.a@gmail.com>
2026-01-16 23:04:52 +0200

    Hero Section - Mobile View

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,24 +1,79 @@
 import React from 'react';
 import { FlipWords } from './FlipWords';
-
+import  { motion } from 'motion/react';
 const HeroText = () => {
+    const words = ["Secure", "Modern", "Unique", "Whimsical", "Scalable"];
+    const variants = {
+        hidden: { opacity: 0, x: -50 },
+        visible: { opacity: 1, x: 0 },
+};
     return (
         <div className='z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text'>
             {/* Desktop View */}
             <div className='flex-col hidden md:flex c-space'>
-                <h1 className='text-4xl font-medium'>Hi, I'm Maram</h1>
+                <motion.h1 
+                className='text-4xl font-medium'
+                variants={variants}
+                initial = "hidden"
+                animate = "visible"
+                transition = {{ delay: 1 }}
+                >Hi, I'm Maram</motion.h1>
                 <div className='flex flex-col items-start'>
-                    <p className='text-5xl font-medium text-neutral-300'>A Lazy Programmer 
-                    <br/> dedicated to crafting</p>
-                    <div>
-                        <FlipWords words={["Secure", "Modern", "Unique", "Whimsical", "Scalable"]} className='font-black text-white text-8xl'/>
-                    </div>
-                    <p className='text-4xl font-medium text-neutral-300'>Web Solutions</p>
+                    <motion.p
+                    className='text-5xl font-medium text-neutral-300'
+                    variants={variants}
+                    initial = "hidden"
+                    animate = "visible"
+                    transition = {{ delay: 1.2 }}
+                    >A Lazy Programmer 
+                    <br/> dedicated to crafting</motion.p>
+                    <motion.div 
+                    variants={variants}
+                    initial = "hidden"
+                    animate = "visible"
+                    transition = {{ delay: 1.4 }}>
+                        <FlipWords words={words} className='font-black text-white text-8xl'/>
+                    </motion.div>
+                    <motion.p
+                    className='text-4xl font-medium text-neutral-300'
+                    variants={variants}
+                    initial = "hidden"
+                    animate = "visible"
+                    transition = {{ delay: 1.6 }}>Web Solutions</motion.p>
                 </div>
             </div>
 
             {/* Mobile View */}
-
+            <div className='flex flex-col md:hidden text-left'>
+                <motion.p
+                className='text-4xl font-medium'
+                variants={variants}
+                initial = "hidden"
+                animate = "visible"
+                transition = {{ delay: 1 }}>Hi, I'm Maram</motion.p>
+                <div>
+                    <motion.p
+                    className='text-5xl font-medium text-neutral-300'
+                    variants={variants}
+                    initial = "hidden"
+                    animate = "visible"
+                    transition = {{ delay: 1.2 }}>A Lazy Programmer 
+                    <br/> dedicated to crafting</motion.p>
+                    <motion.div
+                    variants={variants}
+                    initial = "hidden"
+                    animate = "visible"
+                    transition = {{ delay: 1.4 }}>
+                        <FlipWords words={words} className='font-black text-white text-8xl'/>
+                    </motion.div>
+                    <motion.p
+                    className='text-4xl font-medium text-neutral-300'
+                    variants={variants}
+                    initial = "hidden"
+                    animate = "visible"
+                    transition = {{ delay: 1.6 }}>Web Solutions</motion.p>
+                </div>
+            </div>
         </div>
 
 
---
+++ src/components/ParallexBackground.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ParallexBackground = () => {
+    return (
+        <section className='absolute inset-0 bg-black/40'>
+            <div className='relative h-screen overflow-y-hidden'>
+                {/* Background sky */}
+                <div />
+                {/* Mountain layer 3 */}
+                <div />
+                {/* Planets */}
+                <div />
+                {/* Mountain layer 2 */}
+                <div />
+                {/* Mountain layer 1 */}
+                <div />
+            </div> 
+        </section>
+    )
+}
+
+export default ParallexBackground;
