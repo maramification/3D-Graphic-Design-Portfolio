137337ad97b7cf689894b6f0fd08689bb9a0311c
Maram <maram.hossam.a@gmail.com>
2026-02-18 17:45:26 +0200

    Hero Section - 3D Model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import HeroText from '../components/HeroText'
 import ParallexBackground from '../components/ParallexBackground'
+import { Astronaut } from '../components/Astronaut'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Float, OrbitControls } from '@react-three/drei'
+import { useMediaQuery } from 'react-responsive'
+import { easing } from "maath";
+import Loader from '../components/Loader'
+
 
 const Hero = () => {
+    const isMobile = useMediaQuery({ maxWidth: 853})
     return (
         <section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
             <HeroText/>
             <ParallexBackground/>
+            <figure className='absolute inset-0'
+            style ={{width: "100vw", height: "100vh" }}>
+                <Canvas>
+                    <Suspense fallback={<Loader/>}>
+                    <Float>
+                    <Astronaut scale={isMobile && 0.23}/>
+                    {/* <OrbitControls/> */}
+                    <Rig />
+                    </Float>
+                    </Suspense>
+                </Canvas>
+            </figure>
         </section>
     )
+}
+
+function Rig() {
+    return useFrame((state, delta) => {
+        easing.damp3(
+            state.camera.position,
+            [state.mouse.x/10, 1+state.mouse.y/10, 5],
+            0.5,
+            delta
+        );
+    });
 }
 
 
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             <div className="fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">
                 <div className="mx-auto c-space max-w-7xl">
                     <div className="flex items-center justify-between py-2 sm:py-0">
-                     <a href="/" className="text-xl font-bold transition-colors text-neutral-400 hover:text-white">SFTI</a>
+                     <a href="/" className="text-xl font-bold transition-colors text-neutral-400 hover:text-white">MARAM</a>
                      <button onClick={() => setIsOpen(!isOpen)} className="flex cursor-pointer text-neutral-400 hover:text-white focus:outline-none sm:hidden">
                         <img  src={isOpen? "/assets/menu-close.svg":"/assets/menu-open.svg"} className="w-6 h-6" alt="toggle"/>
                      </button>
