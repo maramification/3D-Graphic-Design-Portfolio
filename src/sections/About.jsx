2f9613b8f52833dec779ff391bef10cd96ce58bb
Maram <maram.hossam.a@gmail.com>
2026-02-28 15:57:39 +0200

    About Section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,26 +1,68 @@
-import React from 'react'
+import { useRef } from 'react'
+import Card from '../components/Card'
+import { Globe } from '../components/Globe';
+import CopyEmailButton from '../components/CopyEmailButton';
+import { Frameworks } from '../components/FrameWorks';
 
 const About = () => {
+    const grid2Container = useRef();
     return (
         <section className='c-space section-spacing'>
             <h2 className='text-heading'>About Me</h2>
             <div className='grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12'>
                 {/* Grid 1 */}
-                <div className='flex items-end grid-default-color grid-1'></div>
-                {/* Grid 2 */}
-                    <div className='grid-default-color grid-2'>
-                        <img src='assets/coding-pov.png'
+                <div className='flex items-end grid-default-color grid-1'>
+                    <img src='assets/coding-pov.png'
                         className='absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]'/>
                         <div className='z-10'>
-                            <p>Hi, I'm Maram</p>
+                            <p className='headtext'>Hi, I'm Maram</p>
+                            <p className='subtext'>A T24 Technical Consultant
+                                with over a year of experience delivering core banking solutions. 
+                                {/* Strong foundation in backend development and RESTful APIs, focused on building clear, reliable systems with structured thinking and attention to detail. */}
+                                </p>
+                        </div>
+                        <div className='absolute inset-x-0 pointer-events-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo'></div>
+                </div>
+                {/* Grid 2 */}
+                    <div className='grid-default-color grid-2' ref={grid2Container}>
+                        <div className='flex items-center justify-center w-full h-full'>
+                            <p className='flex items-end text-5xl text-gray-500'>CODE IS CRAFT</p>
+                            <Card style={{ rotate: "75deg", top: "30%", left: "20%" }} text="SOLID" containerRef={grid2Container}/>
+                            <Card style={{ rotate: "-30deg", top: "60%", left: "45%" }} text="T24" containerRef={grid2Container}/>
+                            <Card style={{ rotate: "90deg", top: "30%", left: "70%" }} text="SRP" containerRef={grid2Container}/>
+                            <Card style={{ rotate: "-45deg", top: "55%", left: "0%" }} text="DESIGN PRINCIPLES" containerRef={grid2Container}/>
+                            <Card style={{ rotate: "30deg", top: "70%", left: "70%" }} image="assets/logos/csharp-pink.png" containerRef={grid2Container}/>
+                            <Card style={{ rotate: "-45deg", top: "70%", left: "25%" }} image="assets/logos/dotnet-pink.png" containerRef={grid2Container}/>
+                            <Card style={{ rotate: "30deg", top: "5%", left: "10%" }} image="assets/logos/blazor-pink.png" containerRef={grid2Container}/>
                         </div>
                     </div>
                 {/* Grid 3 */}
-                    <div className='grid-black-color grid-3'></div>
+                    <div className='grid-black-color grid-3'>
+                        <div className='z-10 w-[50%]'>
+                            <p className='headtext'>Time Zone</p>
+                            <p className='subtext'>Based in Egypt, and open to remote work worldwide.</p>
+                        </div>
+                        <figure>
+                            <Globe/>
+                        </figure>
+                    </div>
                 {/* Grid 4 */}
-                    <div className='grid-special-color grid-4'></div>
+                    <div className='grid-special-color grid-4'>
+                        <div className='flex flex-col items-center justify-center gap-4 size-full'>
+                            <p className='text-center headtext'>Do you want to start a project together?</p>
+                            <CopyEmailButton />
+                        </div>
+                    </div>
                 {/* Grid 5 */}
-                    <div className='grid-default-color grid-5'></div>
+                    <div className='grid-default-color grid-5'>
+                        <div className='z-10 w-[50%]'>
+                            <p className='headtext'>Tech Stack</p>
+                            <p className='subtext'> I specialize in a variety of languages, frameworks, tools and platforms that allow me to build robust and scalable applications and solutions. </p>
+                        </div>
+                        <div className='absolute inset-y-0 md:inset-y-9 w-full h-full start-[%50] md:scale-125'>
+                            <Frameworks />
+                        </div>
+                    </div>
             </div>
         </section>
     )
